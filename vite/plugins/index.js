e9f50632956537da1a336cdc31a6929abc5dd95e
R_h_zero <renhongze@able-elec.com>
2021-09-09 19:25:52 +0800

    安装自动引入插件

--- vite/plugins/index.js
+++ vite/plugins/index.js
@@ -9,6 +9,7 @@ import createMock from './mock'
 import createCompression from './compression'
 import createSpritesmith from './spritesmith'
 import createBanner from './banner'
+import createAutoImport from './auto-import'
 // import createElement from './element'
 
 export default function createVitePlugins(viteEnv, isBuild = false) {
@@ -16,6 +17,7 @@ export default function createVitePlugins(viteEnv, isBuild = false) {
     !isBuild && vitePlugins.push(createRestart())
     vitePlugins.push(createHtml(viteEnv, isBuild))
     vitePlugins.push(createComponents())
+    vitePlugins.push(createAutoImport())
     vitePlugins.push(createSvgIcon(isBuild))
     vitePlugins.push(createMock())
     isBuild && vitePlugins.push(...createCompression(viteEnv))

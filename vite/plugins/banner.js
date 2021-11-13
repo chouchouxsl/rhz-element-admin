b92d634bd85d9b4d549d9da8e14ec0dd716221d0
R_h_zero <renhongze@able-elec.com>
2021-11-13 10:18:33 +0800

    xxx

--- vite/plugins/banner.js
+++ vite/plugins/banner.js
@@ -3,8 +3,7 @@ import banner from 'vite-plugin-banner'
 export default function createBanner() {
     return banner(`
 /**
- * name: Fantastic-admin
- * homepage: https://hooray.gitee.io/fantastic-admin/
+ * name: rhz-admin
  */
     `)
 }
